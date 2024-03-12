191f3c60338717317291cc9bc02933544c1bbc9f
SourLemonJuice <SourLemonJuice233@outlook.com>
2024-03-13 00:16:08 +0800

    是完整可运行的 main.c 耶
    一个有趣的小项目哈哈

---
+++ main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+// 包含所有可能字符的字符串
+char Random_String_List[] = "abcdefghijklmnopqrstuvwxyz";
+
+// 生成指定长度的随机字符串
+char *generate_random_string(int str_Long)
+{
+  char *random_string = malloc(str_Long + 1);
+
+  // 生成随机字符串
+  for (int i = 0; i < str_Long; i++) {
+    // 生成一个随机数
+    int random_index = rand() % (strlen(Random_String_List) - 1);
+
+    // 获取随机字符
+    random_string[i] = Random_String_List[random_index];
+  }
+
+  // 添加字符串结束符
+  random_string[str_Long] = '\0';
+
+  // 返回随机字符串
+  return random_string;
+}
+
+int main()
+{
+  struct timespec ts;
+
+  // 输出14行信息
+  for (int i=1;i<=16;i++)
+  {
+    // 休眠250毫秒
+    ts.tv_sec = 0;
+    ts.tv_nsec = (rand() % (340000000 - 90000000) + 90000000);
+    nanosleep(&ts, NULL);
+
+    // 生成一个长度为3-18的随机字符串
+    int name_String_Long = ( rand() % (13 - 3) + 3 );
+    char *random_string = generate_random_string(name_String_Long);
+
+    printf("\033[32m[  OK  ]\033[0m ");
+    printf("\033[30mstarting\033[0m ");
+    // 打印随机字符串
+    printf("%s\n", random_string);
+  }
+
+  return 0;
+}
